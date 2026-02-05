d4756dbef77dec98002d72316b6d642e56d7acc8
Leodim <leonidasdimakis2@gmail.com>
2026-02-05 14:53:30 +0200

    Add project files.

---
+++ Bookify.Domain/Apartments/Apartment.cs
@@ -0,0 +1,31 @@
+﻿using Bookify.Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookify.Domain.Apartments
+{
+    public sealed class Apartment :Entity
+    {
+        public Apartment(Guid id) : base(id)
+        {
+        }
+
+        public Name Name { get; private set; }
+        public Description Description { get; private set; }
+
+        public Address Address { get; private set; }
+        
+
+       public Money price { get; private set; }
+        public Money CleaningFee { get; private set; }
+
+
+        public DateTime?  LastBookonUtc { get; private set; }
+
+        public List<Amenity> amenities { get; private set; } = new();
+
+    }
+}
